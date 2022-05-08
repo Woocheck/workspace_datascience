f0a3f1c7f121954331921ac0c5216ef0cc22c74f
Woocheck <34909593+Woocheck@users.noreply.github.com>
2022-05-08 13:18:17 +0200

    databace create

--- modules/database.py
+++ modules/database.py
@@ -1,4 +1,21 @@
 
+import sqlite3 
+
+
+
 class localdatabase:
-    def __init__(self) -> None:
-        pass
+    """Class for database handling"""
+    def __init__( self, name ) -> None:
+        try: 
+            dbconnection = sqlite3.connect(name)
+        except:
+            print(" DB connection without success.")
+        else:
+            print("Connection with database is done.")
+        
+        cursor = dbconnection.cursor()
+    
+    def tableCreate( name, columnsDictionary ):
+        """Create new table in database""" 
+        
+        
