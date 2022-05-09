57d87151c2ec21e8b8285bad4c08924c0d400dc1
Woocheck <34909593+Woocheck@users.noreply.github.com>
2022-05-09 20:30:45 +0200

    destructor and destructor

--- modules/database.py
+++ modules/database.py
@@ -1,5 +1,6 @@
 
 import sqlite3
+from sqlite3 import Error
 
 from colorama import Cursor 
 
@@ -7,16 +8,26 @@ from colorama import Cursor
 
 class localdatabase:
     """Class for database handling"""
-    def __init__( self, name ) -> None:
+    def __init__( self, db_file ) -> None:
         try: 
-            dbconnection = sqlite3.connect(name)
+            self.dbconnection = sqlite3.connect(db_file)
         except Error as e:
             print(" DB connection without success.")
             print(e)
         else:
             print("Connection with database is done.")         
         
-        self.cursor = dbconnection.cursor()
+        self.cursor = self.dbconnection.cursor()
+    
+    def __del__(self):
+        try: 
+            self.dbconnection.close()
+        except Error as e:
+            print("Closing a database with a failure.")
+            print(e)
+        else:
+            print("Database shutdown successfully completed.")
+    
     
     def tableCreate(self, tablename, columnsDictionary ):
         """Create new table in database"""        
@@ -26,7 +37,7 @@ class localdatabase:
             order = order + columnsDictionary(key)+' '+key+', '
         
         order = order[:-3] + ')'
-        
-        self.cursor.execute( order )
+        print( "Prepared order:", order )
+        self.cursor.execute( order, self.cursor )
         
         
